d2a6ba517a6042700b412e7155e54e30cfda4bba
AbhiramiiS <abhiramis5002@gmail.com>
2024-07-11 21:28:59 +0530

    add

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
 import logo from './logo.svg';
 import './App.css';
-import Adduser from './components/Adduser';
+import Add from './components/Add';
+import Search from './components/Search';
+import Navbar from './components/Navbar';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import View from './components/View';
 
 function App() {
   return (
     <div>
-      <Adduser></Adduser>
+      <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Add/>}/>
+        <Route path='/navbar' element={<Navbar/>}/>
+        <Route path='/search' element={<Search/>}/>
+        <Route path='/view' element={<View/>}/>
+      </Routes>
+      </BrowserRouter>
     </div>
   );
 }
